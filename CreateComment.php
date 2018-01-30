77302bd3c1a4e9466e724f19ca5878040428e19d
nosyarlin <rayson.ljk@gmail.com>
2018-01-30 22:50:41 +0800

    added articleId to create comment

--- CreateComment.php
+++ CreateComment.php
@@ -3,12 +3,13 @@
 	require_once 'connectors/CommentConnector.php';
 
 	$userId = $_POST['userId'];
+	$articleId = $_POST['articleId'];
 	$content = $_POST['content'];
 	$title = $_POST['title'];
 
 	$CommentConnector = new CommentConnector($conn);
 
-	if(!$CommentConnector->create($userId, $content, $title)) {
+	if(!$CommentConnector->create($userId, $content, $title, $articleId)) {
 		$response['success'] = false;
 		$response['message'] = "Failed to create comment!";
 	}
--- connectors/CommentConnector.php
+++ connectors/CommentConnector.php
@@ -10,6 +10,7 @@
 		public static $COLUMN_DATE_ADDED = "date_added";
 		public static $COLUMN_UPVOTE = "upvote";
 		public static $COLUMN_DOWNVOTE = "downvote";
+		public static $COLUMN_ARTICLEID = "articleId";
 
 
 		private $createStatement = NULL;
@@ -24,14 +25,14 @@
 
 			$this->mysqli = $mysqli;
 
-			$this->createStatement = $mysqli->prepare("INSERT INTO " . CommentConnector::$TABLE_NAME . "(`" . CommentConnector::$COLUMN_USERID . "`,`" . CommentConnector::$COLUMN_CONTENT . "`,`" . CommentConnector::$COLUMN_TITLE . "`) VALUES(?,?,?,?)");
+			$this->createStatement = $mysqli->prepare("INSERT INTO " . CommentConnector::$TABLE_NAME . "(`" . CommentConnector::$COLUMN_USERID . "`,`" . CommentConnector::$COLUMN_CONTENT . "`,`" . CommentConnector::$COLUMN_TITLE . "`,`" . CommentConnector::$COLUMN_ARTICLEID . "`) VALUES(?,?,?,?,?)");
 			$this->selectStatement = $mysqli->prepare("SELECT * FROM " . CommentConnector::$TABLE_NAME . " WHERE `" . CommentConnector::$COLUMN_ID . "` = ?");
 			$this->selectAllStatement = $mysqli->prepare("SELECT * FROM " . CommentConnector::$TABLE_NAME);
 			$this->deleteStatement = $mysqli->prepare("DELETE FROM " . CommentConnector::$TABLE_NAME . " WHERE `" . CommentConnector::$COLUMN_ID . "` = ?");
 		}
 
-		public function create($userId, $content, $title) {
-			$this->createStatement->bind_param("iss", $userId, $content, $title);
+		public function create($userId, $content, $title, $articleId) {
+			$this->createStatement->bind_param("issi", $userId, $content, $title, $articleId);
 			return $this->createStatement->execute();
 		}
 
