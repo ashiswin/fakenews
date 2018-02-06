d8c4c2a6ce64d0ebc959d2917b9419ff502cfb7f
root <root@devostrum.com>
2018-02-06 20:20:47 +0800

    added header lines to GetArticle

--- GetAllArticles.php
+++ GetAllArticles.php
@@ -1,6 +1,10 @@
 <?php
 	require_once 'utils/database.php';
 	require_once 'connectors/ArticleConnector.php';
+	
+	header("Access-Control-Allow-Headers: Content-Type");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Origin: *");
 
 	$ArticleConnector = new ArticleConnector($conn);
 
--- GetArticle.php
+++ GetArticle.php
@@ -1,7 +1,10 @@
 <?php
 	require_once 'utils/database.php';
 	require_once 'connectors/ArticleConnector.php';
-
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	header("Access-Control-Allow-Origin: *");
+	
 	$id = $_GET['id'];
 
 	$ArticleConnector = new ArticleConnector($conn);
