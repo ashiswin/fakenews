2ca7269ff431ec87f81b496825297f1c58c76d9b
Rayson Lim <rayson.ljk@gmail.com>
2018-02-14 10:02:05 +0800

    added error message to GetComment

--- GetComment.php
+++ GetComment.php
@@ -5,20 +5,26 @@
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 	header("Access-Control-Allow-Origin: *");
 
-	$id = $_GET['id'];
-
-	$CommentConnector = new CommentConnector($conn);
-
-	$response['comment'] = $CommentConnector->select($id);
-	$response['success'] = true;	
-
-	// get all the children comments
-	$children_ids = explode(',', $response['comment']['children']);
-	$children = array();
-	foreach ($children_ids as $child) {
-		array_push($children,$CommentConnector->select($child));
+	if (!isset($_GET['id'])) {
+		$response['success'] = false;
+		$response['message'] = "Wrong GET params";
 	}
-	$response['comment']['child_comments'] = $children;
+	else {
+		$id = $_GET['id'];
+
+		$CommentConnector = new CommentConnector($conn);
+
+		$response['comment'] = $CommentConnector->select($id);
+		$response['success'] = true;	
+
+		// get all the children comments
+		$children_ids = explode(',', $response['comment']['children']);
+		$children = array();
+		foreach ($children_ids as $child) {
+			array_push($children,$CommentConnector->select($child));
+		}
+		$response['comment']['child_comments'] = $children;
+	}
 
 	echo(json_encode($response));
 ?>
