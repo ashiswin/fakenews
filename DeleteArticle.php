b1c56445e44cb07cb0d0c182644ab9bd177d9570
nosyarlin <rayson.ljk@gmail.com>
2018-02-20 17:04:26 +0800

    deletes comments with articles

--- DeleteArticle.php
+++ DeleteArticle.php
@@ -1,6 +1,7 @@
 <?php
 	require_once 'utils/database.php';
 	require_once 'connectors/ArticleConnector.php';
+	require_once 'connectors/CommentConnector.php';
 	header("Access-Control-Allow-Headers: Content-Type");
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 	header("Access-Control-Allow-Origin: *");
