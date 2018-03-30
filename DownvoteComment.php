e1d6bd2c2e10175aa969f837ce02e377bfcea631
Isaac Ashwin <ashiswin@gmail.com>
2018-03-30 11:59:46 +0800

    Added downvote comment

--- DownvoteComment.php
+++ DownvoteComment.php
@@ -1,16 +1,20 @@
 <?php
 	require_once 'utils/database.php';
 	require_once 'connectors/CommentConnector.php';
+	require_once 'connectors/CommentVotesConnector.php';
 	header("Access-Control-Allow-Headers: Content-Type");
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 	header("Access-Control-Allow-Origin: *");
 
-	if (isset($_POST['id'])) {
-		$id = $_POST['id'];
+	if (isset($_POST['comment_id'])) {
+		$comment = $_POST['comment_id'];
+		$user = $_POST['user_id'];
+		$upvote = 0;
 
 		$CommentConnector = new CommentConnector($conn);
+		$CommentVotesConnector = new CommentVotesConnector($conn);
 
-		if(!$CommentConnector->updateDownvote($id)) {
+		if(!$CommentConnector->updateDownvote($id) || !$CommentVotesConnector->create($comment, $user, $upvote)) {
 			$response['success'] = false;
 			$response['message'] = "Failed to downvote comment!";
 		}
