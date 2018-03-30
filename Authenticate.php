0348a01f7d087508a9080d9e45cf26a37a17c8bd
Isaac Ashwin <ashiswin@gmail.com>
2018-03-30 12:03:31 +0800

    Added authenticate and getcomment

--- Authenticate.php
+++ Authenticate.php
@@ -2,6 +2,7 @@
 	require_once 'utils/database.php'; // Provides handle to sql session
 	require_once 'connectors/UserConnector.php'; // Gives all the functions related to User
 	require_once 'connectors/ArticleVotesConnector.php';
+	require_once 'connectors/CommentVotesConnector.php';
 	
 	header("Access-Control-Allow-Headers: Content-Type");
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
@@ -15,6 +16,7 @@
 	
 	$UserConnector = new UserConnector($conn);
 	$ArticleVotesConnector = new ArticleVotesConnector($conn);
+	$CommentVotesConnector = new CommentVotesConnector($conn);
 	
 	$result = $UserConnector->selectByEmail($email); // Check if email exists in Database
 
@@ -36,6 +38,12 @@
 			$response["voted_articles"] = array();
 			for($i = 0; $i < count($voted_articles); $i++) {
 				array_push($response['voted_articles'], $voted_articles[$i]['article_id']);
+			}
+			
+			$voted_comments = $CommentVotesConnector->selectByUser($result[UserConnector::$COLUMN_ID]);
+			$response["voted_comments"] = array();
+			for($i = 0; $i < count($voted_comments); $i++) {
+				array_push($response['voted_comments'], $voted_comments[$i]['comment_id']);
 			}
  		}
 		else { // If password does not match
--- GetComment.php
+++ GetComment.php
@@ -1,6 +1,7 @@
 <?php
 	require_once 'utils/database.php';
 	require_once 'connectors/CommentConnector.php';
+	require_once 'connectors/CommentVotesConnector.php';
 	header("Access-Control-Allow-Headers: Content-Type");
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 	header("Access-Control-Allow-Origin: *");
@@ -24,6 +25,12 @@
 			array_push($children,$CommentConnector->select($child));
 		}
 		$response['comment']['child_comments'] = $children;
+		
+		$voters = $CommentVotesConnector->selectByUser($result[UserConnector::$COLUMN_ID]);
+		$response['comment']["comment_voters"] = array();
+		for($i = 0; $i < count($voters); $i++) {
+			array_push($response['comment']["comment_voters"], $voters[$i]['user_id']);
+		}
 	}
 
 	echo(json_encode($response));
